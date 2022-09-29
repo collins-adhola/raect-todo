238025c85469da9c860bbc4b9e2cfad87b8cc614
Collins Adhola <coadhola@gmail.com>
2022-09-29 15:59:19 +0100

    style form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { UserContext } from "./index";
+import React, { useContext, useReducer } from "react";
+import {Button} from 'react-bootstrap';
+import TodoList from './ToDoList'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+const todosInitialState = {
+  todos:[{id:1, text: "finishing writing hooks chapter"},
+  {id:2, text: "play with kids"},
+  {id:3, text: "read bible"}
+]
 }
+
+export const TodosContext = React.createContext()
+
+function todosReducer(state, action){
+  switch(action.type){
+    default:
+      return todosInitialState
+  }
+}
+
+function App(){
+  const [state, dispatch] = useReducer(todosReducer, todosInitialState)
+  return  (
+  <TodosContext.Provider value = {{state, dispatch}}>
+    <TodoList />
+
+  </TodosContext.Provider>
+  )
+}
+
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//++++++++++++++++++++reducer++++++++++++++++++++++++++++++++++
+// const initialState = {
+//   count: 0,
+// };
+
+// function App(props) {
+
+//   function reducer(state, action){
+//     switch(action.type){
+//       case "increment":
+//         return {count: state.count + 1}
+//         case "decrement":
+//           return{count: state.count - 1}
+//           case "reset":
+//             return initialState
+//             default:
+//               return initialState;
+//     }
+//   }
+
+//   const [state, dispatch] = useReducer(reducer, initialState);
+//   return (
+//     <div>
+//       Count: {state.count}
+//       <br/>
+//       <Button onClick={() => dispatch ({type:'increment'})}>
+//         Increment
+//       </Button>
+
+//       <Button variant="secondary" onClick={()=> dispatch({type:'decrement'})}>
+//         Decrement
+//       </Button>
+//       <Button variant= 'success' onClick={()=> dispatch({type:'reset'})}>
+//         Reset
+//       </Button>
+//     </div>
+//   )
+// }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+export const UserContext = React.createContext();
+const username = "Collins";
+
 root.render(
+  // <React.StrictMode>
+  //   <UserContext.Provider value={username}>
+  //     <App  />
+  //   </UserContext.Provider>
+  // </React.StrictMode>
+
   <React.StrictMode>
     <App />
   </React.StrictMode>
